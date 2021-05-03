a8003786cd05d2aa799384b6a92bfc3b705b5cfd
startgeng <2447925377@qq>
2021-05-04 03:09:55 +0800

    并发编程练习  线程安全和线程不安全的注解

---
+++ src/main/java/com/kevin/concurrentcy/interfaces/NoThreadSafe.java
@@ -0,0 +1,18 @@
+package com.kevin.concurrentcy.interfaces;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author kevin
+ * @version 1.0
+ * @date 2021-05-04 3:08
+ */
+@Target(value = ElementType.METHOD)
+@Retention(value = RetentionPolicy.SOURCE)
+public @interface NoThreadSafe {
+
+    String value() default "";
+}
